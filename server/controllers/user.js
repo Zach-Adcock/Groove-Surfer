6a7ebbb54f4f017ee57ef9c436969a9cbd8dcba0
Zach-Adcock <zwa2014@gmail.com>
2022-05-03 22:17:06 -0700

    JWT key made private

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -14,7 +14,7 @@ export const signin = async(req, res) => {
     if (!isPasswordCorrect) return res.status(400).json({message: "Invalid email/password combination."})
 
     //create token to send to front end
-    const token = jwt.sign({ email: existingUser.email, id: existingUser._id }, 'test', { expiresIn: "1h"})
+    const token = jwt.sign({ email: existingUser.email, id: existingUser._id }, process.env.TOKEN_KEY, { expiresIn: "1h"})
     
     res.status(200).json({ result: existingUser, token})
   } catch (err) {
@@ -37,7 +37,7 @@ export const signup = async(req, res) => {
 
   const result = await User.create({ email, password: hashedPassword, name: `${firstName} ${lastName}`})
 
-  const token = jwt.sign({ email: result.email, id: result._id }, 'test', { expiresIn: "1h"});
+  const token = jwt.sign({ email: result.email, id: result._id }, process.env.TOKEN_KEY, { expiresIn: "1h"});
   res.status(200).json({ result, token});
 
  } catch (err) {
--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -8,7 +8,7 @@ const auth = async (req, res, next) => {
     let decodedData;
 
     if (token && isCustomAuth) {
-      decodedData = jwt.verify(token, 'test');
+      decodedData = jwt.verify(token, process.env.TOKEN_KEY);
       req.userId = decodedData?.id;
     } else {
       decodedData = jwt.decode(token);
